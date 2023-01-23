0da8c54db9844cdf2f239b7f1533089ae5a4630e
Airborm <4ayushrana@gmail.com>
2023-01-23 09:04:20 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Service from './components/Service'
+import Contact from './components/Contact'
+import About from './components/About'
+import Career from './components/Career'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App ">
+
+    <div >
+      <NavBar/>
+
+       <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/service" element={<Service/>}></Route>
+            <Route exact path="/about" element={<About/>}></Route> 
+            <Route exact path="/careers" element={<Career/>}></Route> 
+            <Route exact path="/contact" element={<Contact/>}></Route> 
+
+
+        </Routes>
+      
+      
+    </div>
+      
     </div>
   )
 }
---
+++ src/components/Career.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Career(props){
+    return(
+        <div >
+            <h1>Careers</h1>
+        </div>
+       
+
+    )
+}
