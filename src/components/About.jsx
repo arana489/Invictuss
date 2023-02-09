20f8aeaeb970c127d99cc44708018224f9dc455b
Tania Gole <taniagole@Tanias-MacBook-Pro.local>
2023-02-09 14:08:05 -0500

    scrolling feature for about us page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import {useRef} from "react"; 
+import "/src/App.css";
 
+export default function About(props) {
+    const whoWeAre = useRef(null);
+    const whyWorkWithUs = useRef(null);
+    const meetTheTeam = useRef(null);
 
-export default function About(props){
+    const scrollComponent = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop, 
+            behavior: 'smooth',         
+        })
+    }
+
     return(
-        <div >
-            <h1>About</h1>
+        <div>
+        <div class="rotate">
+            <ul>
+                <li onClick={() => scrollComponent(whoWeAre)} className = "link">Who we are</li>
+                <li onClick={() => scrollComponent(meetTheTeam)} className = "link">Meet the team</li>
+                <li onClick={() => scrollComponent(whyWorkWithUs)} className = "link">Why Invictus</li>
+            </ul>      
+        <div ref={whoWeAre} className="whoWeAre">
         </div>
-       
-
-    )
+        <div ref={meetTheTeam} className="meetTheTeam">
+        </div>
+        <div ref={whyWorkWithUs} className="whyWorkWithUs">
+        </div>
+        </div>  
+        </div>
+    );
 }
