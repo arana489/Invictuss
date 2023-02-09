976096a03b7257f37bf43ee0ada78d95350e8115
Jacob Kulik <jakul238@gmail.com>
2023-02-09 17:44:07 -0500

    feature/002-jacob-services-dropdown-test

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,13 +1,64 @@
 import React from "react";
+//import "./src/App.css";
 
 export default function Service(props){
     return(
         <div>
-            <h1>Service</h1>
-            <button id="toggleButton">Toggle Dropdown</button>
-                <div id="textSection" class="hidden">
-                    <h2>testing</h2>
-                </div>
+            <div className="text-8xl text-center">
+                <br></br>
+                <h2>Services</h2>
+                <p className="text-6xl">Description Here</p>
+                <br></br>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 1</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 2</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 3</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 4</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 5</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 6</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 7</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+                <button id="toggleButton">Thing 8</button>
+                    <div id="textSection" class="hidden">
+                        <h2>testing</h2>
+                    </div>
+            </div>
+            
             <script src="ServiceDropdown.jsx"></script>
         </div>
        
