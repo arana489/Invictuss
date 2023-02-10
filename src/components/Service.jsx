333fc98f40a493c2b1981c84006a453688833861
Jacob Kulik <jakul238@gmail.com>
2023-02-09 22:22:10 -0500

    feature/004-jacob-multi-service-drop

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,23 +2,51 @@ import React from "react";
 import ServiceDropdown from "./ServiceDropdown";
 
 export default function Service(props){
-
-    //array of titles
-    // or loop from array
-
+    const services = ['DevSecOps and Cloud Defense',
+                         'SSDLC + Security Awareness',
+                        'CI/CD architecture and optimization',
+                        'Visibility Enhancement',
+                        'On-prem Hardening and Architecture',
+                        'CISO as a Service',
+                        'Web Application Penetration Testing (WAPT)',
+                        'Compliance, Regulation, and GRC']
     return(
         <div>
             <div className="text-8xl text-center">
                 <br></br>
                 <h2>Services</h2>
                 <p className="text-6xl">Description Here</p>
-                <br></br>
             </div>
             
             <div>
-                <ServiceDropdown service="service" description="description"></ServiceDropdown>
+                <ServiceDropdown service={services[0]} description="description"></ServiceDropdown>
             </div>
-            
+            <div>
+                <ServiceDropdown service={services[1]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[2]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[3]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[4]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[5]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[6]} description="description"></ServiceDropdown>
+            </div>
+            <div>
+                <ServiceDropdown service={services[7]} description="description"></ServiceDropdown>
+            </div>
+
+            <div>
+                <br></br>
+                <br></br>
+            </div>
         </div>
        
 
--- src/components/ServiceDropdown.jsx
+++ src/components/ServiceDropdown.jsx
@@ -1,13 +1,14 @@
 import { useState, React } from 'react';
 
+
 const ServiceDropdown = (props) => {
   const { service, description } = props;
 
   //      bool      func        useState hook
   const [visible, setVisible] = useState(false);
   return (
-    <div className="text-4xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
-      <button onClick={() => {setVisible(!visible)}}>{service}</button>
+    <div className="text-4xl text-center sm:text-left sm:pl-16 sm:pt-16">
+      <button class="hover:font-bold" onClick={() => {setVisible(!visible)}}>{service}</button>
       {visible ? <p className="text-2xl">{description}</p> : <></>}
     </div>
   );
