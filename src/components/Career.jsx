efc6a9672b4567d152e8e20b0878bfd3500d4a9b
gilicohen12 <gili8cohen@gmail.com>
2023-02-12 11:29:01 -0500

    colors are back

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Career() {
     return (
         
-        <div class="flex-above bg-blue-900">
+        <div class="flex-above bg-dark-blue">
             <div class="flex">
                 <div class="flex-above w-2/3 p-3">
                     <h1 class="text-center font-bold">
@@ -25,11 +25,11 @@ export default function Career() {
                     </div>
                 </div>
                     <div class="w-1/3 p-10">
-                        <img class=" border-blue-600 border-8 rounded-3xl" src="images/gili.jpg" alt="jake.jpg"></img>
+                        <img class=" border-orange border-8 rounded-3xl" src="images/gili.jpg" alt="jake.jpg"></img>
                     </div>
             </div>
 
-            <div class="rounded-md w-64 h-64 bg-blue-700 flex">
+            <div class="rounded-md w-64 h-64 bg-light-gray flex">
                 <div class="text-white flex text-lg items-center">
                         JOB TITLE</div>
             </div>
