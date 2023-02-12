346bb68785a48c7d3b5a1f0696ad48d0f16079af
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2023-02-12 12:28:33 -0500

    typing animation in call to action

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
+
 
 export default function CallToAction(props) {
     return (
-        <div className="hover:animate-spin  text-4xl md:text-8xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
+        <div className="flex flex-wrap text-4xl md:text-8xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">        <h1 className="text-left text-white">
+          Professional Cyber
+          <br />
+          <span className=" text-light-blue">
+            <TypeAnimation
+              sequence={[
+                'Security',
+                4000,
+                'Protection',
+                4000,
+                'Company',
+                4000
+              ]}
+              cursor={false}
+              repeat={Infinity}
+              style={{ fontSize: '4xl', color: 'light-blue', lineHeight: '1.2' }}
+            />
+          </span> <span className="text-white"> with </span>
 
-            <h1 className="text-left text-white">
-                Professional Cyber
-                <br />
-                <span className=" text-[#5AAFD8]"> Security </span>  with
-                <br />
-                <span className=" text-[#E9993F]"> Reliable </span> Service
-            </h1>
-
-
-        </div>
+          <span className=" text-orange"> Reliable </span> Service
+        </h1>
+      </div>
     )
 }
