ff8cff3792bf7c2da279af3f4f0b7660ebe6e994
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2023-02-12 14:52:21 -0500

    homepage with goal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import CallToAction from "./CallToAction";
 import CEOMessage from "./CEOMessage";
 import Expertise from "./Expertise";
 import Mission from "./Mission";
+import Goal from "./Goal";
 
 export default function Home(props) {
     return (
         <div className="overflow-y-auto flex-auto overflow-hidden w-full h-screen bg-[#04142A]">
             <Mission> </Mission>
             <Expertise></Expertise>
+            <Goal></Goal>
             <CEOMessage></CEOMessage>
         </div >
 
