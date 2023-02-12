d3e584800b4e8968458a088e5708b513014df733
Tania Gole <taniagole@Tanias-MacBook-Pro.local>
2023-02-12 11:23:36 -0500

    new About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,13 @@ export default function About(props) {
     const whyWorkWithUs = useRef(null);
     const meetTheTeam = useRef(null);
 
+    function handleClick () {
+         document.getElementById("test").style.color = "orange";
+         
+        }
+        // document.getElementById("test1").style.color = "orange";
+        // document.getElementById("test2").style.color = "orange";
+
     const scrollComponent = (elementRef) => {
         window.scrollTo({
             top: elementRef.current.offsetTop, 
@@ -18,10 +25,11 @@ export default function About(props) {
         <div>
         <div class="rotate">
             <ul>
-                <li onClick={() => scrollComponent(whoWeAre)} className = "link">Who we are</li>
-                <li onClick={() => scrollComponent(meetTheTeam)} className = "link">Meet the team</li>
-                <li onClick={() => scrollComponent(whyWorkWithUs)} className = "link">Why Invictus</li>
-            </ul>      
+                <li onClick={() => {scrollComponent(whoWeAre); handleClick();}} className = "link" id ="test">Who we are</li>
+                <li onClick={() => {scrollComponent(meetTheTeam); handleClick();}} className = "link" id="test1">Meet the team</li>
+                <li onClick={() => {scrollComponent(whyWorkWithUs); handleClick();}} className = "link" id="test2">Why Invictus</li>
+            </ul>
+            </div>      
         <div ref={whoWeAre} className="whoWeAre">
         </div>
         <div ref={meetTheTeam} className="meetTheTeam">
@@ -29,6 +37,5 @@ export default function About(props) {
         <div ref={whyWorkWithUs} className="whyWorkWithUs">
         </div>
         </div>  
-        </div>
     );
 }
