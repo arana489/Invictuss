5f02b9ded7d1b2f77399f4a223e0cf26ded0a6e9
gilicohen12 <gili8cohen@gmail.com>
2023-02-12 10:34:29 -0500

    snell w kris

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -3,36 +3,39 @@ import React from "react";
 
 export default function Career() {
     return (
-        <div class="flex">
-            <div class="flex-above w-2/3 p-3">
-                <h1 class="text-center font-bold">
-                    THE FUTURE OF CYBER-SECURITY AND THE BRIGHTEST MINDS  </h1>
-                <div class="table-auto">
-                    <table class="w-full">
-                        <tbody>
-                            <tr>
-                                <td class="border px-4 py-8">Row 1, Column 2</td>
-                                <td class="border px-4 py-8">Row 1, Column 3</td>
-                            </tr>
-                            <tr>
-                                <td class="border px-4 py-8">Row 2, Column 2</td>
-                                <td class="border px-4 py-8">Row 2, Column 3</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        
+        <div class="flex-above bg-blue-900">
+            <div class="flex">
+                <div class="flex-above w-2/3 p-3">
+                    <h1 class="text-center font-bold">
+                        THE FUTURE OF CYBER-SECURITY AND THE BRIGHTEST MINDS  </h1>
+                    <div class="table-auto">
+                        <table class="w-full">
+                            <tbody>
+                                <tr>
+                                    <td class="border px-4 py-8">Row 1, Column 2</td>
+                                    <td class="border px-4 py-8">Row 1, Column 3</td>
+                                </tr>
+                                <tr>
+                                    <td class="border px-4 py-8">Row 2, Column 2</td>
+                                    <td class="border px-4 py-8">Row 2, Column 3</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+                    <div class="w-1/3 p-10">
+                        <img class=" border-blue-600 border-8 rounded-3xl" src="images/gili.jpg" alt="jake.jpg"></img>
+                    </div>
             </div>
-                <div class="w-1/3">
-                    <img src="images/gili.jpg" alt="jake.jpg"></img>
-                </div>
-        </div>
 
-                /* <div class="rounded-md w-64 h-64 bg-blue-900">
-            <div class="flex items-center h-full">
-              <p class="text-center">JOB TITLE</p>
-              <p class="text-center">Hidden short description</p>
-            </div> 
-            </div>*/
+            <div class="rounded-md w-64 h-64 bg-blue-700 flex">
+                <div class="text-white flex text-lg items-center">
+                        JOB TITLE</div>
+            </div>
+        </div>
 
     )
 }
+
+          
