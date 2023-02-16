b19cee0c86af8fa88c8154aeb03475736b4e2bcc
annakim <annakim@Annas-MacBook-Pro.local>
2023-02-16 18:58:02 -0500

    update About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,39 +1,32 @@
 import React from "react";
-import {useRef} from "react"; 
+import {useRef} from "react";
 import "/src/App.css";
-
 export default function About(props) {
     const landingPage = useRef(null);
     const whoWeAre = useRef(null);
     const whyWorkWithUs = useRef(null);
     const meetTheTeam = useRef(null);
     const scrollTop = useRef(null);
-
     const[currPage,setCurrPage] = React.useState(false)
-
     const scrollComponent = (elementRef,s) => {
         setCurrPage(s);
         console.log(s)
         window.scrollTo({
-            top: elementRef.current.offsetTop, 
-            behavior: 'smooth',         
+            top: elementRef.current.offsetTop,
+            behavior: 'smooth',
         })
     }
-    
     const styles1 = {
-        color: currPage=="whoWeAre" ? "orange" : "black"
+        color: currPage=="whoWeAre" ? "orange" : "gray"
     }
-
     const styles2 = {
-        color: currPage=="meetTheTeam" ? "orange" : "black"
+        color: currPage=="meetTheTeam" ? "orange" : "gray"
     }
-
     const styles3 = {
-        color: currPage=="whyWorkWithUs" ? "orange" : "black"
+        color: currPage=="whyWorkWithUs" ? "orange" : "gray"
     }
-
 return(
-          <div> 
+          <div>
             <div class="rotate">
              <ul>
                 <li onClick={() => {scrollComponent(whoWeAre,"whoWeAre")}} className = "link" style={styles1}>Who we are</li>
@@ -41,18 +34,17 @@ return(
                 <li onClick={() => {scrollComponent(whyWorkWithUs,"whyWorkWithUs")}} className = "link" id="test2" style={styles3}>Why Invictus</li>
              </ul>
             </div>
-             <div ref={landingPage} className="landingPage">
-            <div>
-                <body>
+            <div ref={landingPage} className="landingPage">
+            <div><body>
                     <p><center><font size="400px">About Us</font></center></p>
                 </body>
+            </div>
             </div>
         <div ref={whoWeAre} className="whoWeAre"></div>
         <div ref={meetTheTeam} className="meetTheTeam"></div>
         <div ref={whyWorkWithUs} className="whyWorkWithUs"></div>
         <div className="static text-right list-none">
-            <li onClick={() => {scrollComponent(whoWeAre); handleClick();}} id ="test">Back To Top</li>         
-        </div>     
+            <li onClick={() => {scrollComponent(landingPage); handleClick();}} id ="test">Back To Top</li>
         </div>
         </div>
     );
