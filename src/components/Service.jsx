e4264b13cdedc7c76aeb3c9a664367816e1d4ea4
Jacob Kulik <jakul238@gmail.com>
2023-02-16 23:13:45 -0500

    service/002-icon-initial-functionality

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -8,16 +8,16 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Service(props){
     // Array with all services, may incorporate a loop later for efficiency and better code
-    const services = [{service: 'DevSecOps and Cloud Defense', description: "Description"},
-                    {service: 'SSDLC + Security Awareness', description: "Description"},
-                    {service: 'CI/CD Architecture and Optimization', description: "Description"},
-                    {service: 'Visibility Enhancement', description: "Description"},
-                    {service: 'On-prem Hardening and Architecture', description: "Description"},
-                    {service: 'CISO as a Service', description: "Description"},
-                    {service: 'Web App Penetration Testing', description: "Description"},
-                    {service: 'Compliance, Regulation, and GRC', description: "Description"}]
+    const services = [{service: 'DevSecOps and Cloud Defense', description: "We will help you to 'Shift Left' and implement security  across all levels of development.", d:"M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"},
+                    {service: 'SSDLC + Security Awareness', description: "We will secure your entire software development pipeline, starting from the first line of code, to the last deployment in Production.", d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"},
+                    {service: 'CI/CD Architecture and Optimization', description: "We will show you how you can have a robust pipeline, and also have it be as efficient and secure as possible.", d:"M21.75 17.25v-.228a4.5 4.5 0 00-.12-1.03l-2.268-9.64a3.375 3.375 0 00-3.285-2.602H7.923a3.375 3.375 0 00-3.285 2.602l-2.268 9.64a4.5 4.5 0 00-.12 1.03v.228m19.5 0a3 3 0 01-3 3H5.25a3 3 0 01-3-3m19.5 0a3 3 0 00-3-3H5.25a3 3 0 00-3 3m16.5 0h.008v.008h-.008v-.008zm-3 0h.008v.008h-.008v-.008z"},
+                    {service: 'Visibility Enhancement', description: "We will show you how you can have a robust pipeline, and also have it be as efficient and secure as possible.",d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"},
+                    {service: 'On-prem Hardening and Architecture', description: "Our architectural design will provide a comprehensive defense strategy across the board: IT, IOT and OT, and allow you to shorten your TTM (Time to Market).",d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"},
+                    {service: 'CISO as a Service', description: "We provide information security leadership, security guidance to senior management and lead the organization’s information security program, thus reducing your managerial overhead.",d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"},
+                    {service: 'Web App Penetration Testing', description: "We will show you how to avoid deploying “swiss-cheese” apps, by hermetically sealing your applications, and continuously check for new vulnerabilities.",d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"},
+                    {service: 'Compliance, Regulation, and GRC', description: "We will assist you in lining-up your environment to either regulation standard you may require, so that you can be sure you are always compliant.",d:"M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"}]
 
-    const serviceList=services.map((x) => <ServiceDropdown service={x.service} description={x.description}></ServiceDropdown>)
+    const serviceList=services.map((x) => <ServiceDropdown service={x.service} description={x.description} d={x.d}></ServiceDropdown>)
  
     return(
         <div>
@@ -34,14 +34,6 @@ export default function Service(props){
             <div className="flex justify-evenly">
                 <div className="flex flex-wrap justify-evenly gap-3">
                     {serviceList}
-                    {/* <ServiceDropdown service={services[0]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[1]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[2]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[3]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[4]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[5]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[6]} description="description"></ServiceDropdown>
-                    <ServiceDropdown service={services[7]} description="description"></ServiceDropdown> */}
                 </div>
             </div>
             
--- src/components/ServiceDropdown.jsx
+++ src/components/ServiceDropdown.jsx
@@ -2,15 +2,20 @@ import { useState, React } from 'react';
 
 
 const ServiceDropdown = (props) => {
-  const { service, description } = props;
+  const { service, description, d } = props;
 
   //      bool      func        useState hook
   const [visible, setVisible] = useState(false);
   return (
-    <div className="text-4xl sm:text-left sm:pl-8 sm:pt-8 sm:pr-8 sm:pb-8 rounded-lg border-2 w-1/4 text-justify border-dark-blue">
-      {/* Button is equal to the service param and hides/unhides the description */}
-      <button class="hover:text-light-blue" onClick={() => {setVisible(!visible)}}>{service}</button>
-      {visible ? <p className="text-2xl">{description}</p> : <></>}
+    <div className="text-2xl font-semibold text-clip overflow-hidden justify-center sm:text-center sm:pr-4 sm:pl-4 sm:pt-8 sm:pb-8 rounded-lg border-2 w-1/4 text-justify border-dark-blue">
+      <div class="hover:text-light-blue hover:stroke-light-blue" onClick={() => {setVisible(!visible)}}>
+        <svg fill="none" className="stroke-darkest-blue" stroke-width="1.5" viewBox="0 0 24 24" width="25%" height="25%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d={d}></path>
+        </svg>
+        {/* Button is equal to the service param and hides/unhides the description */}
+        <button>{service}</button>
+      </div>
+      {visible ? <p className="text-xl font-normal"><br></br>{description}</p> : <></>}
     </div>
   );
  };
