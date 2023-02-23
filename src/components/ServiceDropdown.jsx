afc84339664fb2e858779526249f76dd18d91017
Launna Atkinson <lsatk3@yahoo.com>
2023-02-23 18:00:22 -0500

    Icons

--- src/components/ServiceDropdown.jsx
+++ src/components/ServiceDropdown.jsx
@@ -8,8 +8,8 @@ const ServiceDropdown = (props) => {
   const [visible, setVisible] = useState(false);
   return (
     <div className="text-2xl font-semibold text-clip overflow-hidden justify-center sm:text-center sm:pr-4 sm:pl-4 sm:pt-8 sm:pb-8 rounded-lg border-2 w-1/4 text-justify border-dark-blue">
-      <div class="hover:text-light-blue hover:stroke-light-blue" onClick={() => {setVisible(!visible)}}>
-        <svg fill="none" className="stroke-darkest-blue" stroke-width="1.5" viewBox="0 0 24 24" width="25%" height="25%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+      <div class="hover:text-light-blue hover:stroke-light-blue " onClick={() => {setVisible(!visible)}}>
+        <svg fill="none"  className="stroke-darkest-blue" stroke-width="1.5" viewBox="0 0 24 24" width="15%" height="15%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d={d}></path>
         </svg>
         {/* Button is equal to the service param and hides/unhides the description */}
