612836be65fcfe01205d9ad3d50d6920b6a21a57
Jacob Kulik <jakul238@gmail.com>
2023-02-23 19:25:04 -0500

    service/004-service-centering-justification

--- src/components/ServiceDropdown.jsx
+++ src/components/ServiceDropdown.jsx
@@ -7,15 +7,15 @@ const ServiceDropdown = (props) => {
   //      bool      func        useState hook
   const [visible, setVisible] = useState(false);
   return (
-    <div className="text-2xl font-semibold text-clip overflow-hidden justify-center sm:text-center sm:pr-4 sm:pl-4 sm:pt-8 sm:pb-8 rounded-lg border-2 w-1/4 text-justify border-dark-blue">
-      <div class="hover:text-light-blue hover:stroke-light-blue " onClick={() => {setVisible(!visible)}}>
-        <svg fill="none"  className="stroke-darkest-blue" stroke-width="1.5" viewBox="0 0 24 24" width="15%" height="15%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <div className="text-sm md:text-xl lg:text-2xl font-semibold justify-center text-center pr-4 pl-4 pt-8 pb-8 rounded-lg border-2 w-1/3 md:w-1/4  border-dark-blue">
+      <div class="stroke-darkest-blue text-darkest-blue hover:text-light-blue hover:stroke-light-blue" onClick={() => {setVisible(!visible)}}>
+        <svg fill="none"  className="mx-auto" stroke-width="1.5" viewBox="0 0 24 24" width="30%" height="30%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d={d}></path>
         </svg>
         {/* Button is equal to the service param and hides/unhides the description */}
-        <button>{service}</button>
+        <button class="mx-auto">{service}</button>
       </div>
-      {visible ? <p className="text-xl font-normal"><br></br>{description}</p> : <></>}
+      <div class={`${visible ? "max-h-96" : "max-h-0"} transition-all duration-300 text-xs md:text-sm lg:text-xl overflow-hidden`}>{visible && description}</div>
     </div>
   );
  };
