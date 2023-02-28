e7994984d120d5f4adbd6a9d850c48b62f1ffc8b
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2023-02-28 10:37:46 -0500

    moved intro text

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -4,8 +4,8 @@ import { TypeAnimation } from 'react-type-animation';
 
 export default function CallToAction(props) {
     return (
-        <div className="flex flex-wrap text-4xl md:text-8xl pl-10 pt-10 text-center sm:text-left sm:pl-16 sm:pt-16">
-            <h1 className="text-left text-white">
+        <div className="flex flex-col text-4xl md:text-7xl pl-10 pt-20 md:pt-40 text-center sm:text-left sm:pl-16 sm:pt-40 md:flex-wrap md:flex-row md:items-center">
+  <h1 className="text-left text-white md:w-3/4 lg:w-1/2">
                 Professional Cyber
                 <br />
                 <span className=" text-light-blue">
