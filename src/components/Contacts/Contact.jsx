168458ad40b9033a9c82ab37bf424d33839b2f57
Kris Amerman <amerman.k@northeastern.edu>
2023-03-14 10:16:29 -0400

    fix html attribute error, use textarea, and margin styles

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -42,39 +42,39 @@ export default function Contact(){
 
 
                 <div className="bg-contactBlue lg:m-0 m-auto w-[30rem] md:w-[35rem] lg:w-[35rem] h-[38rem] rounded-3xl flex items-center justify-center">
-                    <form>
+                    <form id="contactform">
                         <div className="grid grid-cols-2 gap-[2rem] mb-[3rem]  ">
                             <div className="flex flex-col gap-[2rem]">
                                 <div>
-                                    <label className="text-bright-orange text-sm" for="name" > Name</label><br></br>
-                                        <input type="text" value={info.name} id="name" className="placeholder-gray-500 p-2.5 text-l h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, name:e.target.value}))}></input><br></br>
+                                    <label className="text-bright-orange text-sm" htmlFor="name" > Name</label><br></br>
+                                    <input type="text" value={info.name} id="name" className="my-2 placeholder-gray-500 p-2.5 text-l h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, name:e.target.value}))}></input><br></br>
                                 </div>
 
                                 <div>
-                                    <label className="text-bright-orange text-sm" for="email">Email</label><br></br>
-                                        <input type="text" value={info.email} id="email" className="p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, email:e.target.value}))}></input><br></br>
+                                    <label className="text-bright-orange text-sm" htmlFor="email">Email</label><br></br>
+                                    <input type="text" value={info.email} id="email" className="my-2 p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, email:e.target.value}))}></input><br></br>
                                 </div>
                             </div>
 
                             <div className="flex flex-col gap-[29px]">
                                 <div>
-                                    <label className="text-bright-orange text-sm" for="services">Services</label><br></br>
-                                        <select type="text" value={info.services} id="phone" className="p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, services:e.target.value}))}>  
-                                            <option value="service 1">service 1</option>
-                                            <option value="service 2">service 2</option>
-                                            <option value="service 3">service 3</option>
-                                            <option value="service 4 ">service 4</option>  
-                                        </select><br></br>
+                                    <label className="text-bright-orange text-sm" htmlFor="services">Services</label><br></br>
+                                    <select type="text" value={info.services} id="phone" className="my-2 p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px rounded-full" onChange={e=>setInfo((x)=>({...x, services:e.target.value}))}>  
+                                        <option value="service 1">service 1</option>
+                                        <option value="service 2">service 2</option>
+                                        <option value="service 3">service 3</option>
+                                        <option value="service 4 ">service 4</option>  
+                                    </select><br></br>
                                 </div>
                                 <div className="">
-                                    <label className="text-bright-orange text-sm" for="company">Company</label><br></br>
-                                        <input type="text" id="company" value={info.company} className="p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px] rounded-full" onChange={e=>setInfo((x)=>({...x, company:e.target.value}))}></input><br></br>
+                                    <label className="text-bright-orange text-sm" htmlFor="company">Company</label><br></br>
+                                    <input type="text" id="company" value={info.company} className="my-2 p-2.5 text-sm  h-[45px] w-[180px] md:w-[230px] lg:[230px] rounded-full" onChange={e=>setInfo((x)=>({...x, company:e.target.value}))}></input><br></br>
                                 </div>
                             </div>
                         </div>
                         <div className="mb-[29px]">
-                            <label className="text-bright-orange text-sm" for="message">Message</label><br></br>
-                                <input type="text" id="message" value={info.message} className="p-2.5 text-sm h-[128.64px] md:w-[482.56px] lg:md:w-[482.56px] w-[400px] rounded-2xl " onChange={e=>setInfo((x)=>({...x, message:e.target.value}))}></input><br></br>
+                            <label className="text-bright-orange text-sm" htmlFor="message">Message</label><br></br>
+                            <textarea name="message" form="contactform" value={info.message} className="my-2 p-2.5 text-sm h-[128.64px] md:w-[482.56px] lg:md:w-[482.56px] w-[400px] rounded-2xl " onChange={e => setInfo((x) => ({ ...x, message: e.target.value }))}></textarea><br></br>
                         </div>
                         <div>
                             <button type="button" id ="submit" onClick ={submit} className="p-2.5 text-s mt-4 h-[45px] w-[160px] rounded-full bg-orange"> Send Message</button>
