b1e1aa7f0f5bc4e17754a6b50eaaab29616df04d
Kris Amerman <amerman.k@northeastern.edu>
2023-03-14 09:57:52 -0400

    add map key for testimonialList

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -4,10 +4,12 @@ import TestimonyBox from "./TestimonyBox";
 
 export default function Testimonials() {
 
-    const testimonals =[{name:"Rikesh",role:"DevOps Manager" ,text:"“Invictuss helped us integrate security into our ENTIRE development process, including the code writing itself, alongside with establishing security guilds and nominating “champions” from within the coding teams, leading to improved security and raised awareness. Their team provided us with more than knowledge. They changed the way we work, for the best!”"},
-                        {name:"Emily",role:"Production Engineer", text:"“Invictuss understands the importance of modern technology in today's business landscape. With their help, we were able to implement modern OT solutions that have transformed our company. The new technology has greatly improved our efficiency and reduced downtime, which has had a positive impact on our revenue. Their expertise in OT is unmatched, and I would highly recommend their services.”"}]  
+  const testimonals =[{name:"Rikesh",role:"DevOps Manager" ,text:"“Invictuss helped us integrate security into our ENTIRE development process, including the code writing itself, alongside with establishing security guilds and nominating “champions” from within the coding teams, leading to improved security and raised awareness. Their team provided us with more than knowledge. They changed the way we work, for the best!”"},
+                      {name:"Emily",role:"Production Engineer", text:"“Invictuss understands the importance of modern technology in today's business landscape. With their help, we were able to implement modern OT solutions that have transformed our company. The new technology has greatly improved our efficiency and reduced downtime, which has had a positive impact on our revenue. Their expertise in OT is unmatched, and I would highly recommend their services.”"}]  
 
-    const testimonalList=testimonals.map((x) => <TestimonyBox name={x.name} text={x.text} role={x.role}/>)
+  const testimonalList=testimonals.map((x, i) => (
+    <TestimonyBox key={i} name={x.name} text={x.text} role={x.role}/>
+  ))
 
 
   return (
