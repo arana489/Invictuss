0b51a31f6b5a072637018fd2839953d7062616ac
Evan Cook <cook.evank@gmail.com>
2023-03-19 15:54:46 -0400

    make initial changes to branch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Testimonials from "./Testimonials";
 import CallToAction from "./CallToAction";
-import HomeValues from "./HomeValues"
+import HomeValues from "./HomeValues";
 
 export default function Home(props) {
-    return (
-        <div className="overflow-y-auto flex-auto overflow-hidden w-full h-screen bg-blue-bg">
-            <div className="flex">
-                <CallToAction/>
-                <img className="object-scale-down w-[0rem] md:w-[18rem] lg:w-[18rem]  ml-auto mr-24 mt-14" src = "src/assets/HomePic.svg" alt="Invictuss logo"/>
-            </div>
-            
-            <HomeValues/>
-            <Testimonials/>
-        </div >
+  return (
+    <div className="overflow-y-auto flex-auto overflow-hidden w-full h-screen bg-blue-bg">
+      <div className="flex">
+        <CallToAction />
+        <img
+          className="object-scale-down w-[0rem] md:w-[18rem] lg:w-[18rem]  ml-auto mr-24 mt-14"
+          src="src/assets/HomePic.svg"
+          alt="Invictuss logo"
+        />
+      </div>
 
-
-    )
+      <HomeValues />
+      <Testimonials />
+    </div>
+  );
 }
