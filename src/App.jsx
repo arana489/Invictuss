c681c6900d9ce779a07cb75feca2357388271f72
Airborm <4ayushrana@gmail.com>
2023-03-23 22:04:03 -0400

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Route,Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
