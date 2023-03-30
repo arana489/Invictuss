5d91231148703337d277e576dbbb2599b9330cfe
Airborm <4ayushrana@gmail.com>
2023-03-30 13:48:56 -0700

    d

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Employee from "./Employee"
+import dictionary from '../../dictionary.json'
+
+
 export default function About(props) {
+    const ab=dictionary.About
+    const employees = ab.MeetOurTeam.employees
+
+    const employeeList= employees.map(x=><Employee className=""imageSrc={x.image} eName={x.name} title= {x.role} description={x.description}></Employee>)
+
+
 return(
+
     <div className="h-500% ">
-    <h1 className="text-white text-lg ml-16 mt-10">Our Approach</h1>
+    <h1 className="text-white text-lg ml-16 mt-10">{ab.OurApproach.header}</h1>
             {/* our approach section */}
             <div className="flex items-center ml-14 mt-10 ">
                     <div className="flex gap">
                         <p className="text-center tracking-tight mr-[10%] lg:w-[60%] text-white font-xs md:text-m lg:text-m">
-                            Invictuss is a CyberSecurity company with a unique 360 approach. We offer a "Shift-Left" approach to prevent security issues and design vulnerabilities. Our strategy reduces incidents and maximizes early detection, optimizing resources spent on security. We have almost 40 years of experience securing highly-sensitive information environments, Operation Technology (OT), cyber defense systems, and software development. Our experience covers strategy, architecture, integration, DevOps, and automation.
+                            {ab.OurApproach.info}
                         </p>
                         <img class="object-scale-down w-[0rem]  lg:w-[20rem] mt-[-5%] lg:mr-14" src="/CareersPage.svg" alt="jake.jpg"></img>
                     </div>
@@ -18,8 +28,7 @@ return(
         <div>
             <h1 className="text-white text-lg ml-16 mt-10">Meet Our Team</h1>
             <div className="flex s:flex-col s:items-center md:flex lg:flex gap-x-24 justify-center mt-8">
-                <Employee className=""imageSrc="/Marom.jpg" eName="Marom" title= "Cheif Executive Officer" description="My name is Marom, and I bring to the table DevSecOps expertise and vast IT knowledge, which allows me to bring a unique perspective to our team."></Employee>
-                <Employee className=""imageSrc="/Liran.jpg" eName="Liran" title= "Cheif Operations Officer" description="I am proud to be part of a team that is dedicated to providing best cyber services to all of our customers. My expertise lies in information security and networking protocols, as well as Operational Technology"></Employee>
+                {employeeList}
             </div>
 
 
@@ -27,21 +36,15 @@ return(
         <hr class="h-px ml-16 mr-16 my-8 text-white border-t-2 "></hr>
             
         <div>
-            <h1 className="text-white text-lg ml-16 mt-10">Meet From Our CEO</h1>
+            <h1 className="text-white text-lg ml-16 mt-10">{ab.MessageFromOurCEO.header}</h1>
             <div className="flex text-left">
                 <div className="mt-8 mb-8 px-20">
                     <p className="tracking-tight text-white text-m md:text-lg  lg:text-m">
-                    &ldquo;Invictuss provided exceptional service in securing our cloud
-                    infrastructure. Their team of experts were able to identify and
-                    mitigate several vulnerabilities that could have resulted in a
-                    major security breach. They were also able to provide practical
-                    solutions that were easy to implement and maintain. We highly
-                    recommend their services to any company looking to secure their
-                    cloud infrastructure&rdquo;
+                    {ab.MessageFromOurCEO.quote}
                     </p>
                     <div className="text-white py-6">
-          <p className="text-white text-md">Marom</p>
-          <p className="text-md text-light-gray">Cheif Executive Officer</p>
+          <p className="text-white text-md">{ab.MessageFromOurCEO.name}</p>
+          <p className="text-md text-light-gray">{ab.MessageFromOurCEO.role}</p>
         </div>
                 </div>
     </div>
--- src/components/Home/CallToAction.jsx
+++ src/components/Home/CallToAction.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { TypeAnimation } from 'react-type-animation';
-
+import dictionary from '../../dictionary.json'
 
 export default function CallToAction(props) {
+
+    const Home = dictionary.Home
     return (
         <div className="flex text-xl md:text-xl ml-10 mt-24 text-center sm:text-left sm:pl-16 ">
             <h1 className="text-left font-extrabold text-white">
-                Professional Cyber
+                {Home.CallToAction.top}
                 <br />
                 <span className=" text-light-blue">
                     <div className="h-14">
                     <TypeAnimation
-                        sequence={[
-                            'Security',
-                            3000,
-                            'Protection',
-                            3000,
-                            'Company',
-                            3000
-                        ]}
+                        sequence={Home.CallToAction.middle}
                         cursor={false}
                         repeat={Infinity}
                         style={{ fontSize: '4xl', color: 'light-blue', lineHeight: '1.2' }}
@@ -26,7 +21,7 @@ export default function CallToAction(props) {
                     </div>
                     
                 </span> 
-                <span className="text-orange"> with Reliable Service</span>
+                <span className="text-orange"> {Home.CallToAction.bottom}</span>
             </h1>
         </div>
     )
