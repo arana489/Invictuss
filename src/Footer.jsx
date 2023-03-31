444c2f799b5d3e5b5a8e1032d3a0a90adb93e201
Airborm <4ayushrana@gmail.com>
2023-03-30 17:43:48 -0700

    update all page to use json and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,8 +5,18 @@ import {Link} from "react-router-dom"
 export default function Footer(props){
     //nav bar organizes all the links
     return(
-        <nav className="flex drop-shadow-lg text-light-blue font-extrabold  bg-navBg h-24 items-center ">
-            <img className="object-scale-down ml-6 w-[9rem]" src = "/InvictussLogo.svg" alt="Invictuss logo"/>
+        <nav className="md:flex lg:flex drop-shadow-lg text-light-blue font-extrabold  bg-navBg h-24 items-center ">
+            <img className="object-scale-down ml-[40%] md:ml-6 lg:ml-6 mr-auto w-[9rem]" src = "/InvictussLogo.svg" alt="Invictuss logo"/>
+            <div className="flex gap-x-4 md:mr-14 lg:mr-14 ml-[45%] mt-2 md:mt-0 lg:mt-0" >
+                <div className="hover:bg-light-gray hover:rounded p-[2px]">
+                    <a href="https://www.linkedin.com/company/invictuss/" target="_blank"><img className="object-scale-down w-[1.75rem]"src="./Linkedin.svg" ></img></a>
+                </div>
+                <div className="hover:bg-light-gray hover:rounded p-[2px]">
+                    <a href="https://api.whatsapp.com/send?phone=972505156080&text=Hi" target="_blank"><img className="object-scale-down w-[1.75rem]"src="./whatsapp.svg" ></img></a>
+                </div>
+            </div>
+            
+
 
         </nav>
        
--- src/components/Careers/CareerBox.jsx
+++ src/components/Careers/CareerBox.jsx
@@ -28,7 +28,7 @@ const CareerBox = (props) => {
           <div style={{ position: 'absolute', bottom: '20px', right: '30px'}}> {/* Add absolute positioning to child div */}
           <button className="text-white border border-white rounded-full flex items-center w-32 h-10" id='button_col'>
       <img src="/Arrow.svg" alt="Arrow" class="h-6 w-6 ml-5" />
-      <a className = "flex-grow text-right mr-6" href="mailto:cv@invictuss.io" > Apply</a>
+      <a className = "flex-grow text-right mr-6" href="mailto:sales@invictuss.io" > Apply</a>
               </button>
               </div>
         </div>
@@ -41,7 +41,7 @@ const CareerBox = (props) => {
         <div style={{ position: "absolute", bottom: "20px", right: "30px" }}>
           <button className="text-white border border-white rounded-full flex items-center w-32 h-10" id='button_col'>
             <img src="/Arrow.svg" alt="Arrow" class="h-6 w-6 ml-5" />
-            <a className = "flex-grow text-right mr-6" href="mailto:cv@invictuss.io"> Apply</a>
+            <a className = "flex-grow text-right mr-6" href="mailto:sales@invictuss.io"> Apply</a>
           </button>
         </div>
       </div>
