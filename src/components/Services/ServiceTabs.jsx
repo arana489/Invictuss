bfe53bb8a12fd25c79d622d31e293ce1c3565627
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2023-04-02 16:36:00 -0400

    created hover colors for buttons

--- src/components/Services/ServiceTabs.jsx
+++ src/components/Services/ServiceTabs.jsx
@@ -10,15 +10,15 @@ function ServiceTabs() {
 
     return(
         <div className="text-center">
-            <div className="border-b-8 pt-20 pb-5 text-white text-m flex justify-center gap-x-10 md:gap-x-14 lg:gap-x-24 font-lg">
-                <button onClick={() => handleServiceSelect("DevSecOps")}>DevSecOps</button>
-                <button onClick={() => handleServiceSelect("SSDLC")}>SSDLC</button>
-                <button onClick={() => handleServiceSelect("CI/CD Architecture")}>CI/CD Architecture</button>
-                <button onClick={() => handleServiceSelect("Visibility Enhancement")}>Visibility Enhancement</button>
-                <button onClick={() => handleServiceSelect("On-Prem Hardening")}>On-Prem Hardening</button>
-                <button onClick={() => handleServiceSelect("CISO as a Service")}>CISO as a Service</button>
-                <button onClick={() => handleServiceSelect("PT")}>PT</button>
-                <button onClick={() => handleServiceSelect("Compliance")}>Compliance</button>
+            <div className="border-b-8 border-white pt-20 pb-5 text-m flex justify-center gap-x-6 md:gap-x-10 lg:gap-x-20 font-lg">
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("DevSecOps")}>DevSecOps</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("SSDLC")}>SSDLC</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("CI/CD Architecture")}>CI/CD Architecture</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("Visibility Enhancement")}>Visibility Enhancement</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("On-Prem Hardening")}>On-Prem Hardening</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("CISO as a Service")}>CISO as a Service</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("PT")}>PT</button>
+                <button className="text-white hover:text-orange" onClick={() => handleServiceSelect("Compliance")}>Compliance</button>
             </div>
             <ServiceContentUnit service={selectedService} name="test"/>
         </div>
