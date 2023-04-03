fb7388e7f54eab3397e0c0bffa62e3b70758f8db
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2023-04-03 16:50:07 -0400

    final draft of tab bar! sexy version

--- src/components/Services/ServiceTabs.jsx
+++ src/components/Services/ServiceTabs.jsx
@@ -12,31 +12,55 @@ function ServiceTabs() {
     return(
         <div>
             <div className="text-center mb-20 ml-10 mr-10 transform opacity-0 animate-slide-up" style={{ "--delay": "1" }}>
-                <div className="border-b-8 border-white pt-20 pb-5 text-m flex justify-center gap-x-6 md:gap-x-10 lg:gap-x-20 font-lg"
+                <div className="pt-20 pb-5 text-m flex justify-center font-lg"
                      style={{ '--text-color': 'var(--orange)' }}>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "DevSecOps" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "DevSecOps" ? "orange" : "",
+                                borderBottomColor: selectedService === "DevSecOps" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("DevSecOps")}>DevSecOps</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "SSDLC" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "SSDLC" ? "orange" : "",
+                                borderBottomColor: selectedService === "SSDLC" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("SSDLC")}>SSDLC</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "CI/CD Architecture" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "CI/CD Architecture" ? "orange" : "",
+                                borderBottomColor: selectedService === "CI/CD Architecture" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("CI/CD Architecture")}>CI/CD Architecture</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "Visibility Enhancement" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "Visibility Enhancement" ? "orange" : "",
+                                borderBottomColor: selectedService === "Visibility Enhancement" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("Visibility Enhancement")}>Visibility Enhancement</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "On-Prem Hardening" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "On-Prem Hardening" ? "orange" : "",
+                                borderBottomColor: selectedService === "On-Prem Hardening" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("On-Prem Hardening")}>On-Prem Hardening</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "CISO as a Service" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "CISO as a Service" ? "orange" : "",
+                                borderBottomColor: selectedService === "CISO as a Service" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("CISO as a Service")}>CISO as a Service</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "PT" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "PT" ? "orange" : "",
+                                borderBottomColor: selectedService === "PT" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("PT")}>PT</button>
-                    <button className="text-white hover:text-orange transition duration-500 ease-in-out"
-                            style={{ color: `${selectedService === "Compliance" ? 'orange' : ''}` }}
+                    <button className="p-8 border-b-8 border-white hover:border-orange text-white hover:text-orange transition duration-500 ease-in-out"
+                            style={{
+                                color: selectedService === "Compliance" ? "orange" : "",
+                                borderBottomColor: selectedService === "Compliance" ? "orange" : ""
+                              }}
                             onClick={() => handleServiceSelect("Compliance")}>Compliance</button>
                 </div>
             </div>
